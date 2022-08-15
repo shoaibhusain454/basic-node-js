eaba9c1e5897442f0f41340e931282f20bb91f79
shoaibhusain454 <shoaib.husain@infobeans.com>
2022-08-15 18:31:19 +0530

    small changes

--- app/models/index.js
+++ app/models/index.js
@@ -1,4 +1,4 @@
-const UserModel = require("./user/userModel")
+const UserModel = require("./user/TserModel")
 const TaskModel = require("./task/TaskModel")
 // const AddressModel = require("./user/addressModel")
 // const FriendsModel = require("./user/friendsModel")
